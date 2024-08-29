6710a789fbfb5b91b651b35fa744c4466ed7dbb3
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-08-29 11:09:53 +0530

    port number change

--- Server/server.py
+++ Server/server.py
@@ -29,4 +29,4 @@ def upload_pdf():
     return jsonify({"error": "Invalid file type, only PDF allowed"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=3000)
